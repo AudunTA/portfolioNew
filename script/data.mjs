bc0dd9ef82296e7bee1c9f72d3c4d20c42af19d7
AudunTA <aud-and2@online.no>
2022-12-21 01:17:31 +0100

    added script

---
+++ script/data.mjs
@@ -0,0 +1,9 @@
+export const data = {
+  title: "AuctionHouse",
+  description: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque est
+    reprehenderit libero? Laudantium blanditiis aperiam harum at
+    assumenda voluptate debitis reprehenderit exercitationem reiciendis
+    perferendis aliquam dolores, libero voluptas. Atque commodi quia
+    porro voluptatum accusantium corrupti iusto maiores fuga modi in?`,
+  link: "linkhref",
+};
