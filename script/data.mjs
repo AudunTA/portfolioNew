4acd0482261f7bdc2320692c3fbebf783b2832de
AudunTA <aud-and2@online.no>
2023-01-04 16:31:51 +0100

    added about

--- script/data.mjs
+++ script/data.mjs
@@ -1,24 +1,18 @@
-export const data = [{
-  title: "AuctionHouse",
-  description: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque est
-    reprehenderit libero? Laudantium blanditiis aperiam harum at
-    assumenda voluptate debitis reprehenderit exercitationem reiciendis
-    perferendis aliquam dolores, libero voluptas. Atque commodi quia
-    porro voluptatum accusantium corrupti iusto maiores fuga modi in?`,
-  link: "linkhref",
-}, {
-  title: "FrontFace",
-  description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque es
-  reprehenderit libero  Laudantium blanditiis aperiam harum at
-  assumenda voluptate debitis reprehenderit exercitationem reiciendis
-  perferendis aliquam dolores`,
-  link: "link href"
-},
-{
-  title: "Adventure trail hike",
-  description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque es
-  reprehenderit libero  Laudantium blanditiis aperiam harum at
-  assumenda voluptate debitis reprehenderit exercitationem reiciendis
-  perferendis aliquam dolores`,
-  link: "link href"
-}];
+export const data = [
+  {
+    title: "AuctionHouse",
+    description: `AuctionHouse is my semesterproject for the 3rd semester at Noroff. this project is built with Bootstrap framework and JavaScript`,
+    imgsrc: "/images/auctionHouse.png",
+  },
+  {
+    title: "FrontFace",
+    description: `I developed FrontFace, a social media platform, 
+    in collaboration with a classmate as a course assignment in JavaScript 2. The project was completed within a tight timeframe of one week.`,
+    imgsrc: "/images/frontface.png",
+  },
+  {
+    title: "Adventure trail hike",
+    description: `As a project for my first semester at Noroff, I created Adventure Trails Hike, a fully functional website built with HTML and CSS.`,
+    imgsrc: "/images/adventureTrails.png",
+  },
+];
--- script/index.mjs
+++ script/index.mjs
@@ -1,3 +1,22 @@
 import { data } from "./data.mjs";
 
 console.log(data);
+
+const container = document.querySelector(".wrapper-cards");
+
+for (let i = 0; i < data.length; i++) {
+  container.innerHTML += `
+  <div class="card">
+  <div class="top">
+    <img src="${data[i].imgsrc}" class="img-card" />
+    <h2>${data[i].title}</h2>
+    <p>
+      ${data[i].description}
+    </p>
+  </div>
+
+  <a href="spesific.html?site=${data[i].title}"
+  ><button id="project-seeMore">See More</button></a
+>
+</div>`;
+}
