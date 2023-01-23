f5bfc8ae428d80bf05c567764103d23c05181033
Audun <aud-and2@online.no>
2023-01-23 13:42:00 +0100

    added new project

--- script/data.mjs
+++ script/data.mjs
@@ -1,14 +1,16 @@
 export const data = [
   {
-    title: "Comming soon",
-    description: `this is the current project im working on, comming soon`,
+    title: "Adventure Trails Hike - re-design",
+    description: `This is a redsign of Adventure Trails Hike, here i focues on writing better and clean HTML and CSS, and making components easy to globally change on all pages. slight use of JavaScript for the navigation`,
     imgsrc: "/images/AH-new.png",
-    gitlink: "",
+    liveLink: "https://admirable-parfait-746c8d.netlify.app/index.html",
+    gitlink: "https://github.com/AudunTA/ATH",
   },
   {
     title: "AuctionHouse",
     description: `AuctionHouse is my semesterproject for the 3rd semester at Noroff. this project is built with Bootstrap, JavaScript and SASS`,
     imgsrc: "/images/auctionHouse.png",
+    liveLink: "https://audunta.github.io/SemesterAssignment_Auctionhouse/",
     gitlink: "https://github.com/AudunTA/SemesterAssignment_Auctionhouse",
   },
   {
@@ -16,17 +18,20 @@ export const data = [
     description: `I developed FrontFace, a social media platform, 
     in collaboration with a classmate as a course assignment in JavaScript 2. The project was completed within a tight timeframe of one week.`,
     imgsrc: "/images/frontface.png",
+    liveLink: "",
     gitlink: "",
   },
   {
     title: "Adventure trail hike",
     description: `As a project for my first semester at Noroff, I created Adventure Trails Hike, a fully functional website built with HTML and CSS.`,
+    liveLink: "https://splendorous-figolla-9d9850.netlify.app/",
     imgsrc: "/images/adventureTrails.png",
     gitlink: "https://github.com/AudunTA/adventureTrailHikes",
   },
   {
     title: "PlantSpire",
     description: `This was my semester 2 project at Noroff. This showcased alot of the newly learned javascript and it was graded a B.`,
+    liveLink: "https://unrivaled-mooncake-14dedc.netlify.app/",
     imgsrc: "/images/plantSpire.png",
     gitlink: "https://github.com/AudunTA/PlantSpire_p",
   },
--- script/index.mjs
+++ script/index.mjs
@@ -5,6 +5,12 @@ console.log(data);
 const container = document.querySelector(".wrapper-cards");
 
 for (let i = 0; i < data.length; i++) {
+  let link = "";
+  if (data[i].liveLink) {
+    link = `<a href=${data[i].liveLink} class="live-link">Live site</a>`;
+  } else {
+    console.log("nope");
+  }
   container.innerHTML += `
   <div class="card">
   <div class="top">
@@ -13,10 +19,11 @@ for (let i = 0; i < data.length; i++) {
     <p>
       ${data[i].description}
     </p>
+    ${link}
   </div>
 
   <a href="${data[i].gitlink}"
-  ><button id="project-seeMore">See More</button></a
+  ><button id="project-seeMore">View on github</button></a
 >
 </div>`;
 }
